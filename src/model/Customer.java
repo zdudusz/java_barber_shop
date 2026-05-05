3d57ea7d02ce16316db6133fb48b0bd655ef83a3
zdudusz <eduardogamese8@gmail.com>
2026-05-04 21:24:03 -0300

    cleaning github cache

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,5 +1,5 @@
-public class Client extends User {
-    public Client(String name) {
+public class Customer extends User {
+    public Customer(String name) {
         super(name,false);
     } //construtor passando adm como false para o usuario não ter acesso às permissões adm
 }
